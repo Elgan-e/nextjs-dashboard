d90311b8aa2ad1aeaa1d0a2aa91fead39ec96376
Elgan-e <lgd24pkc@bangor.ac.uk>
2024-12-04 12:03:26 +0000

    changes

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,6 @@
 import { sql } from '@vercel/postgres';
+import { db } from '@vercel/postgres';
+
 import {
   CustomerField,
   CustomersTableType,
@@ -13,13 +15,14 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    const client = await db.connect();
 
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
+    const data = await client.sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -31,7 +34,7 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+      SELECT invoices.amount, customers.name, customers.image_url, customers.email
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
